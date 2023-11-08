ac9f5a4a33e82d6822b07565ee876d87644be06a
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-11-08 13:27:12 +0900

    todoList 생성 및 UI까지 보여주기

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const todoInput = document.querySelector('.new-todo');
+const todoListWrap = document.querySelector('.todo-list');
 let todoList = [];
 todoInput.addEventListener('keydown', (e) => {
   if(!todoInput.value) return;
@@ -9,17 +10,28 @@ todoInput.addEventListener('keydown', (e) => {
     }
     todoList.push(responseTodo);
     window.localStorage.setItem('todoList', JSON.stringify(todoList));
-    loadList();
     todoInput.value = '';
+    createTodo(todoList.length-1);
   }
 })
 
-const loadList = () => {
-  const listFromStorage = JSON.parse(localStorage.getItem('todoList'));
-  console.log('눌림')
-  console.log(listFromStorage)
+let listFromStorage = JSON.parse(localStorage.getItem('todoList'));
+todoList = listFromStorage;
+if(listFromStorage != null){
   for(let i = 0; i <= listFromStorage.length; i++){
+    createTodo(i);
   }
 }
 
-loadList();
+function createTodo(num){
+  const currentTodo = todoList[num];
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <div class="${currentTodo.isSelected ? 'completed' : 'view'}">
+      <input class="toggle" type="checkbox" />
+        <label>${currentTodo.text}</label>
+      <button class="destroy"></button>
+    </div>
+  `;
+  todoListWrap.prepend(li)
+}
