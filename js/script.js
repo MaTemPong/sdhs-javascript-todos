f7f583545a34526f4d027827f670f38c05b54e9b
MaTemPong <107092881+MaTemPong@users.noreply.github.com>
2023-11-10 16:15:13 +0900

    삭제 구현

--- js/script.js
+++ js/script.js
@@ -1,37 +1,55 @@
+
+const getLocal = () => JSON.parse(localStorage.getItem('todoList')) || [];
+const setLocal = (data) => localStorage.setItem('todoList', JSON.stringify(data));
+
 const todoInput = document.querySelector('.new-todo');
-const todoListWrap = document.querySelector('.todo-list');
+let todoListWrap = document.querySelector('.todo-list');
 let todoList = [];
 todoInput.addEventListener('keydown', (e) => {
   if(!todoInput.value) return;
   if(e.key === 'Enter'){
     let responseTodo = {
       text: todoInput.value,
-      isSelected: false
+      isSelected: false,
+      idx: todoList.length
     }
+    console.log(responseTodo.idx)
     todoList.push(responseTodo);
-    window.localStorage.setItem('todoList', JSON.stringify(todoList));
+    setLocal(todoList);
     todoInput.value = '';
     createTodo(todoList.length-1);
   }
-})
+});
 
-let listFromStorage = JSON.parse(localStorage.getItem('todoList'));
-todoList = listFromStorage;
-if(listFromStorage != null){
-  for(let i = 0; i <= listFromStorage.length; i++){
-    createTodo(i);
+reloadTodo(); 
+
+
+function reloadTodo(){
+  todoList = getLocal();
+  if(todoList != null){
+    for(let i = 0; i < todoList.length; i++){
+      createTodo(i);
+    }
   }
 }
 
 function createTodo(num){
   const currentTodo = todoList[num];
   const li = document.createElement('li');
+  if(currentTodo.isSelected)
+    li.classList.add(currentTodo.isSelected)
   li.innerHTML = `
-    <div class="${currentTodo.isSelected ? 'completed' : 'view'}">
+    <div class="view">
       <input class="toggle" type="checkbox" />
         <label>${currentTodo.text}</label>
       <button class="destroy"></button>
     </div>
   `;
-  todoListWrap.prepend(li)
+  todoListWrap.prepend(li);
+  li.querySelector('.destroy').addEventListener('click', ()=>{
+    li.remove();
+    todoList.splice(currentTodo.idx, 1);
+    setLocal(todoList);
+  });
+  li.querySelector('.toggle').addEventListener('checked', ()=>{currentTodo.isSelected = true})
 }
